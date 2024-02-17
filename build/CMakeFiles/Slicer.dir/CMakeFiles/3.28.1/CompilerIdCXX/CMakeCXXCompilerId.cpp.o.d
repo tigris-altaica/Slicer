f227a6ee1b44ae1db4562a729d604233d7c810fa
Alex <alex03089935@gmail.com>
2024-02-17 09:11:22 -0500

    Deleted build and .vscode dirs

--- build/CMakeFiles/Slicer.dir/CMakeFiles/3.28.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Slicer.dir/CMakeFiles/3.28.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/kali/VSCodeProjects/Slicer/build/CMakeFiles/3.28.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kali/VSCodeProjects/Slicer
-# Build directory: /home/kali/VSCodeProjects/Slicer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
